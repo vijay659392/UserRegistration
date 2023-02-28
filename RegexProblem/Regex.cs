e15eb9592c53178bfcd51900a9987b8bad167644
vijay <vijaydurkikar1@gmail.com>
2023-02-28 15:35:33 +0530

    [vijay]Add:User Need To Enter Valid First Name

---
+++ RegexProblem/Regex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexProblem
+{
+    public class RegexMethod
+    {
+      
+         public void ValidateFirstName(string name)
+         {
+            //Vijay
+             string firstName = "^[A-Z][a-z]{3,}?";
+             if(Regex.IsMatch(name,firstName))
+             {
+                Console.WriteLine("First name macthing with Regex");
+             }
+            else
+            {
+                Console.WriteLine("First name not macthing with Regex");
+            }
+         }
+
+  
+    }
+    
+}
